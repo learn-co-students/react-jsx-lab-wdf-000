d7652ee57c0bc0294beec50a5c31719352d9ec32
irevived1 <haifeng.wu@jjay.cuny.edu>
2016-10-10 13:46:29 -0400

    Done.

---
+++ components/FillerText.js
@@ -0,0 +1,12 @@
+const React = require('react');
+const ReactDOM = require('react-dom');
+
+class FillerText extends React.Component {
+  render() {
+    return ( <p>I am a filler text. I can be used to fill your screen. Amazing!</p>);
+  }
+}
+
+module.exports = FillerText;
+
+
---
+++ components/RegistrationForm.js
@@ -0,0 +1,15 @@
+const React = require('react');
+const ReactDOM = require('react-dom');
+
+class RegistrationForm extends React.Component{
+  render() {return (
+    <form action="#" method="get" accept-charset="utf-8">
+      <input type="text" />
+      <input type="password" />
+      <button type="submit">Stuff</button>
+      </form>
+  );}
+  }
+
+
+module.exports = RegistrationForm;
