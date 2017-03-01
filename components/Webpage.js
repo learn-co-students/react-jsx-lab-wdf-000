6d11cafc798220d6a9819a1591d048731124d389
sami4thewin <samiomarmohammed@gmail.com>
2017-03-01 10:16:53 -0500

    working on the webpage

---
+++ components/Webpage.js
@@ -0,0 +1,11 @@
+const filler = require('./FillerText');
+
+class Webpage extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>The world's coolest webpage</h1>
+      </div>
+    )
+  }
+}
